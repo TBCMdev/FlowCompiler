749152047581f40ab5163162929b6692906f641c
TheBigCringeMaster <65819239+deadgunnerYT@users.noreply.github.com>
2022-02-14 19:12:17 +0000

    FIRST COMMIT (read)
    added a readfile func in entry.h header, so we can instantly start the tokenizer. we should use an external library for the tokenizer in my opinion.

--- main/entry.cpp
+++ main/entry.cpp
@@ -8,6 +8,7 @@ using namespace flow;
 
 bool cmd = true;
 bool debug = true;
+bool instantCompile = false;
 int main(){
     if (cmd){
         string path = getUserIN("compile file path: ");
@@ -19,6 +20,16 @@ int main(){
             main();
         }
         if (debug) cout << "reading: \n\n" + cont << endl;
+
+        if (!instantCompile){
+            if (tolower((getUserIN("run after compile? (Y/N): ")) == "Y")){
+                //TODO: run file after compiling is done.
+            }
+        }else{
+            //instant compile
+            
+        }
+
     }
     return 0;
 }
